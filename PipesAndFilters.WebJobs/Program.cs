83d45bb599d77026c7d6e9b2d523bb875e17a2b6
shibayan <shiba-yan@outlook.com>
2015-01-14 22:06:09 +0900

    initial commit

---
+++ PipesAndFilters.WebJobs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Azure.WebJobs;
+
+namespace PipesAndFilters.WebJobs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new JobHost();
+
+            host.RunAndBlock();
+        }
+
+        public static void FirstStage([QueueTrigger("first")] string inbound, [Queue("second")] out string outbound)
+        {
+            outbound = "first " + inbound;
+        }
+
+        public static void SecondStage([QueueTrigger("second")] string inbound, [Queue("third")] out string outbound)
+        {
+            outbound = "second " + inbound;
+        }
+
+        public static void ThirdStage([QueueTrigger("third")] string inbound)
+        {
+            Console.WriteLine("third " + inbound);
+        }
+    }
+}
---
+++ StaticContentHosting/UrlHelperExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace StaticContentHosting
+{
+    public static class UrlHelperExtension
+    {
+        public static string StaticContent(this UrlHelper urlHelper, string contentPath)
+        {
+            return ConfigurationManager.AppSettings["StaticContentBaseUrl"] + urlHelper.Content(contentPath);
+        }
+    }
+}
