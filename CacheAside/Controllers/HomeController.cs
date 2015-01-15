68775ac7b503c4db549dd553fb5c2ebf504c4f20
shibayan <shiba-yan@outlook.com>
2015-01-15 21:51:10 +0900

    fix cache-aside

--- CacheAside/Controllers/HomeController.cs
+++ CacheAside/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Runtime.Caching;
 using System.Web.Mvc;
 
 namespace CacheAside.Controllers
@@ -11,7 +9,26 @@ namespace CacheAside.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            // キャッシュに存在するか確認する
+            var date = _cache.Get("cachekey");
+
+            if (date == null)
+            {
+                // キャッシュに無かったので新しく追加する
+                date = DateTime.Now.ToString();
+
+                _cache.Add("cachekey", date, DateTimeOffset.Now.AddSeconds(10));
+            }
+
+            ViewBag.Message = date;
+
             return View();
         }
+
+        // InProc なキャッシュを使う
+        private static readonly ObjectCache _cache = MemoryCache.Default;
+
+        // Redis をキャッシュに使う場合はこっち
+        //private static readonly ObjectCache _cache = new RedisCacheClient.RedisCache("CONNECTION_STRING");
     }
 }
